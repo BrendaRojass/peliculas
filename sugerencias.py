20b3b6e262e2be87ca2f30ee893d0a42d77b2ef7
Brenda <br7588653@gmail.com>
2026-09-07 10:06:10 -0300

    Add files via upload

---
+++ sugerencias.py
@@ -0,0 +1,29 @@
+print("=============================\n")
+print("   Sugerencias de peliculas  \n ")
+print("=============================\n")
+usuario=input("Buen dia, ¿Cual es tu nombre? ")
+print("¿Que queres ver hoy, "+usuario+"?")
+nombre_pelicula="Las Guerreras Kpop"
+genero_pelicula="Accion"
+anio_pelicula=2025
+rating_pelicula=7.4
+nombre_pelicula2="Troya"
+genero_pelicula2="Accion"
+anio_pelicula2=2004
+rating_pelicula2=7.4
+nombre_pelicula3="Spiderman"
+genero_pelicula3="Accion"
+anio_pelicula3=2002
+rating_pelicula3=7.4
+nombre_pelicula4="¿Y donde esta el piloto?"
+genero_pelicula4="Comedia"
+anio_pelicula4=1980
+rating_pelicula4=7.7
+nombre_pelicula5="Son como niños"
+genero_pelicula5="Comedia"
+anio_pelicula5=2010
+rating_pelicula5=6.0
+print("---- GENEROS ----")
+print("Accion")
+print("Comedia")
+genero_favorito=input("¿Que genero te gusta? ")
